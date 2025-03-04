9507b19ae356b494ffa1047fefca8c3968d16410
nathaniamnbe <nathanael1084@gmail.com>
2025-03-04 20:50:12 +0700

    4. Debugging tests

--- codelab-android-compose-main (1)/codelab-android-compose-main/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ codelab-android-compose-main (1)/codelab-android-compose-main/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,6 +1,8 @@
 package com.example.compose.rally
 
+import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import org.junit.Rule
 import org.junit.Test
@@ -22,6 +24,8 @@ class TopAppBarTest {
             )
         }
 
-        Thread.sleep(5000)
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertIsSelected()
     }
 }

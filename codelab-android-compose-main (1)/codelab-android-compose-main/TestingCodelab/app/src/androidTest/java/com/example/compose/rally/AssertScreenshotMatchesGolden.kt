4c33820102c771e6838c641d56f3ee95e8374030
nathaniamnbe <nathanael1084@gmail.com>
2025-03-04 20:45:30 +0700

    Testing in Jetpack Compose

--- codelab-android-compose-main (1)/codelab-android-compose-main/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/AssertScreenshotMatchesGolden.kt
+++ codelab-android-compose-main (1)/codelab-android-compose-main/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/AssertScreenshotMatchesGolden.kt
@@ -16,65 +16,89 @@
 
 package com.example.compose.rally
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Build
-import androidx.compose.ui.graphics.asAndroidBitmap
-import androidx.compose.ui.test.SemanticsNodeInteraction
-import androidx.compose.ui.test.captureToImage
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.MainTestClock
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.unit.dp
 import androidx.test.filters.SdkSuppress
-import androidx.test.platform.app.InstrumentationRegistry
-import java.io.FileOutputStream
+import com.example.compose.rally.ui.components.AnimatedCircle
+import com.example.compose.rally.ui.theme.RallyTheme
+import org.junit.Rule
+import org.junit.Test
 
 /**
- * Simple on-device screenshot comparator that uses golden images present in
- * `androidTest/assets`. It's used to showcase the [AnimationClockTestRule] used in
- * [AnimatingCircleTests].
+ * Test to showcase [MainTestClock] present in [ComposeTestRule]. It allows for animation
+ * testing at specific points in time.
  *
- * Minimum SDK is O. Densities between devices must match.
+ * For assertions, a simple screenshot testing framework is used. It requires SDK 26+ and to
+ * be run on a device with 420dpi, as that the density used to generate the golden images
+ * present in androidTest/assets. It runs bitmap comparisons on device.
  *
- * Screenshots are saved on device in `/data/data/{package}/files`.
+ * Note that different systems can produce slightly different screenshots making the test fail.
  */
+@ExperimentalTestApi
 @SdkSuppress(minSdkVersion = Build.VERSION_CODES.O)
-fun assertScreenshotMatchesGolden(
-    goldenName: String,
-    node: SemanticsNodeInteraction
-) {
-    val bitmap = node.captureToImage().asAndroidBitmap()
+class AnimatingCircleTests {
 
-    // Save screenshot to file for debugging
-    saveScreenshot(goldenName + System.currentTimeMillis().toString(), bitmap)
-    val golden = InstrumentationRegistry.getInstrumentation()
-        .context.resources.assets.open("$goldenName.png").use { BitmapFactory.decodeStream(it) }
+    @get:Rule
+    val composeTestRule = createComposeRule()
 
-    golden.compare(bitmap)
-}
-
-private fun saveScreenshot(filename: String, bmp: Bitmap) {
-    val path = InstrumentationRegistry.getInstrumentation().targetContext.filesDir.canonicalPath
-    FileOutputStream("$path/$filename.png").use { out ->
-        bmp.compress(Bitmap.CompressFormat.PNG, 100, out)
+    @Test
+    fun circleAnimation_idle_screenshot() {
+        composeTestRule.mainClock.autoAdvance = true
+        showAnimatedCircle()
+        assertScreenshotMatchesGolden("circle_done", composeTestRule.onRoot())
     }
-    println("Saved screenshot to $path/$filename.png")
-}
 
-private fun Bitmap.compare(other: Bitmap) {
-    if (this.width != other.width || this.height != other.height) {
-        throw AssertionError("Size of screenshot does not match golden file (check device density)")
+    @Test
+    fun circleAnimation_initial_screenshot() {
+        compareTimeScreenshot(0, "circle_initial")
     }
-    // Compare row by row to save memory on device
-    val row1 = IntArray(width)
-    val row2 = IntArray(width)
-    for (column in 0 until height) {
-        // Read one row per bitmap and compare
-        this.getRow(row1, column)
-        other.getRow(row2, column)
-        if (!row1.contentEquals(row2)) {
-            throw AssertionError("Sizes match but bitmap content has differences")
+
+    @Test
+    fun circleAnimation_beforeDelay_screenshot() {
+        compareTimeScreenshot(499, "circle_initial")
+    }
+
+    @Test
+    fun circleAnimation_midAnimation_screenshot() {
+        compareTimeScreenshot(600, "circle_100")
+    }
+
+    @Test
+    fun circleAnimation_animationDone_screenshot() {
+        compareTimeScreenshot(1500, "circle_done")
+    }
+
+    private fun compareTimeScreenshot(timeMs: Long, goldenName: String) {
+        // Start with a paused clock
+        composeTestRule.mainClock.autoAdvance = false
+
+        // Start the unit under test
+        showAnimatedCircle()
+
+        // Advance clock (keeping it paused)
+        composeTestRule.mainClock.advanceTimeBy(timeMs)
+
+        // Take screenshot and compare with golden image in androidTest/assets
+        assertScreenshotMatchesGolden(goldenName, composeTestRule.onRoot())
+    }
+
+    private fun showAnimatedCircle() {
+        composeTestRule.setContent {
+            RallyTheme {
+                AnimatedCircle(
+                    modifier = Modifier.background(Color.White).size(320.dp),
+                    proportions = listOf(0.25f, 0.5f, 0.25f),
+                    colors = listOf(Color.Red, Color.DarkGray, Color.Black)
+                )
+            }
         }
     }
-}
-
-private fun Bitmap.getRow(pixels: IntArray, column: Int) {
-    this.getPixels(pixels, 0, width, 0, column, width, 1)
 }
